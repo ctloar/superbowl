921caa5128a65206c2cf36eb9970b538b045cff7
mountaindew33 <joshblad99@gmail.com>
2022-12-09 22:34:29 -0700

    Edit and delete functionality

--- superbowlodds/urls.py
+++ superbowlodds/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from .views import indexPageView, gamesPageView, teamsPageView, votingPageView
 from .views import simulationSelectPageView, simulationDisplayPageView, historyPageView
+from .views import showSingleGamePageView, updateGamesPageView, deleteGamePageView
 
 urlpatterns = [
     path('games/', gamesPageView, name='games'),  
     path('history/', historyPageView, name='history'),  
     path('simulation/', simulationSelectPageView, name='simulationSelect'),
+    path("showSingleGame/<int:game_id>/", showSingleGamePageView, name="single_game"),
+    path("updateGames/", updateGamesPageView, name="update_game"),
+    path('gameDelete/<int:game_id>/', deleteGamePageView, name='delete_game'),
     # path('simulation/<int:team_id1>/<int:team_id2>/', simulationDisplayPageView, name='simulationDisplay'),
     # path('simulation/<int:team_id1>/', simulationDisplayPageView, name='simulationDisplay'),
     path('teams/', teamsPageView, name='teams'),
