c5ad00799770fcf405c7efa0c23b61ce3ac8c904
ctloar <cl464@byu.edu>
2022-12-10 16:13:31 -0700

    railway deploy

--- superbowl/settings.py
+++ superbowl/settings.py
@@ -11,17 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 # import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ns@yhq+p+(s(#05#^+=9%oqeor3&fl0kq4mm#ah69p7%gf72_l'
+# SECRET_KEY = 'django-insecure-ns@yhq+p+(s(#05#^+=9%oqeor3&fl0kq4mm#ah69p7%gf72_l'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
